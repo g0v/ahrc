0fa690b1ead08c2de79bc2241a9f4dab58b5b3c5
YuTin <yuting1987@gmail.com>
2019-06-08 17:08:00 +0900

    feat(web): frist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import About from "./About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    lang: "CH",
+  };
+
+  onLangClick = lang => this.setState({ lang });
+
+  render() {
+    const { lang } = this.state;
+
+    return (
+      <div className="App">
+        <Header onClick={this.onLangClick} />
+        <About lang={lang} />
+      </div>
+    );
+  }
 }
-
-export default App;
